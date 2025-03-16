8f9eb17f34428898a9e888c63156e2a7b84688d3
mohamed-ashif-m <mohamedashif.ai23@stellamaryscoe.edu.in>
2025-03-16 15:28:03 +0530

    config update

--- event-register/vite.config.js
+++ event-register/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Event-Register/", // Ensure this matches your GitHub repo name
+
 })
